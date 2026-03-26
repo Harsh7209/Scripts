f27136cf4c91d6abd04d897ea00f41c9c731e7a3
Harsh Choubey <harshchoubat759@gmail.com>
2026-03-26 20:05:41 +0530

    Added script to download docker and docker compose

---
+++ docker-and-dockercompose-download.sh
@@ -0,0 +1,10 @@
+# script to install docker and docker compose and also add the ubuntu in docker group .. just run chmod +x <This script> 
+
+
+sudo apt update && sudo apt install -y ca-certificates curl gnupg lsb-release && \
+sudo install -m 0755 -d /etc/apt/keyrings && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && \
+sudo systemctl enable docker && sudo systemctl start docker && \
+sudo usermod -aG docker $USER && newgrp docker
